3c6a55ca86bc67d5cc859a70f71023d881c950f9
albertogioia93 <albertogioia@hotmail.it>
2023-03-15 15:55:39 +0100

    collegamento tra div id=app e mount di main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// collegamento globale al div con id=app del file index.html
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      
+    }
+  }
+}).mount('#app')
