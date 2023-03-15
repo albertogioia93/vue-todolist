49fc5ec8030c84ea8bd1131bd1494f46f3f4adb0
albertogioia93 <albertogioia@hotmail.it>
2023-03-15 18:11:58 +0100

    esercizio finito con le 3 milestone richieste

--- js/main.js
+++ js/main.js
@@ -1,12 +1,38 @@
 'use strict';
 
-// collegamento globale al div con id=app del file index.html
-const { createApp } = Vue
+const { createApp } = Vue;
 
 createApp({
-  data() {
-    return {
-      
-    }
-  }
-}).mount('#app')
+    data() {
+        return {
+            todos: [
+                { text: 'Fare la spesa', done: false },
+                { text: 'Studiare Vue.js', done: true },
+                { text: 'Preparare la cena', done: false },
+                { text: 'Lavarsi i denti', done: true },
+            ],
+            newTodo: '',
+        };
+    },
+    methods: {
+        addTodo() {
+            if (this.newTodo.trim() !== '') {
+                this.todos.push({ text: this.newTodo.trim(), done: false });
+                this.newTodo = '';
+            }
+        },
+        removeTodo(index) {
+            this.todos.splice(index, 1);
+        },
+        toggleDone(index) {
+            this.todos[index].done = !this.todos[index].done;
+        },
+    },
+    computed: {
+        remainingTodos() {
+            return this.todos.filter(todo => !todo.done);
+        },
+    },
+}).mount('#app');
+
+
